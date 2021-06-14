b6ceb4393c1f4167f1267afeef4d62034d0b0434
Naseem Oyebola <naseemoyebola@gmail.com>
2021-06-15 00:55:39 +0100

    feat: layout improvement

--- Fundall/Fundall/Login/WelcomeBackViewController.swift
+++ Fundall/Fundall/Login/WelcomeBackViewController.swift
@@ -8,7 +8,12 @@
 import UIKit
 
 class WelcomeBackViewController: UIViewController {
-
+    @IBOutlet weak var userAvatar: UIImageView!
+    @IBOutlet weak var userGreeting: UILabel!
+    @IBOutlet weak var userEmailLabel: UILabel!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var secureButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,5 +21,17 @@ class WelcomeBackViewController: UIViewController {
     }
     
 
-   
+    @IBAction func eyeButtonTapped(_ sender: UIButton) {
+        passwordField.isSecureTextEntry.toggle()
+        let imageName = passwordField.isSecureTextEntry ? "eye.slash" : "eye"
+        secureButton.setImage(UIImage(systemName: imageName), for: .normal)
+    }
+    
+    @IBAction func loginPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func createAccountPressed(_ sender: UIButton) {
+        
+    }
 }
--- Fundall/Fundall/Signup/SignupViewController.swift
+++ Fundall/Fundall/Signup/SignupViewController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class SignupViewController: UIViewController {
-
+    
+    @IBOutlet weak var firstName: UITextField!
+    @IBOutlet weak var lastName: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var phoneNumber: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var eyeSecureButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func eyeSecureTapped(_ sender: UIButton) {
+        passwordField.isSecureTextEntry.toggle()
+        let imageName = passwordField.isSecureTextEntry ? "eye" : "eye.slash"
+        eyeSecureButton.setImage(UIImage(systemName: imageName), for: .normal)
+    }
+    
+    @IBAction func inviteCode(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func signupPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func loginPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func termsConditionPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func privacyPolicyPressed(_ sender: UIButton) {
+        
+    }
 }
