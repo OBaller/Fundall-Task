2d1e08b7b0c1d9fa4c020c11335442c0a5a63c3d
Naseem Oyebola <naseemoyebola@gmail.com>
2021-06-19 15:12:27 +0100

    feat: connect screens

--- Fundall/Fundall/Home/HomepageViewController.swift
+++ Fundall/Fundall/Home/HomepageViewController.swift
@@ -8,22 +8,18 @@
 import UIKit
 
 class HomepageViewController: UIViewController {
-
+    @IBOutlet weak var homeAvatar: UIImageView!
+    @IBOutlet weak var balance: UILabel!
+    @IBOutlet weak var income: UILabel!
+    @IBOutlet weak var amountSpent: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        navigationController?.navigationBar.isHidden = true
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func dashboardPressed(_ sender: UIButton) {
     }
-    */
-
+    
 }
--- Fundall/Fundall/Login/Lifestyle/LifestyleViewController.swift
+++ Fundall/Fundall/Login/Lifestyle/LifestyleViewController.swift
@@ -8,13 +8,23 @@
 import UIKit
 
 class LifestyleViewController: UIViewController {
-
+    @IBOutlet weak var firstName: UILabel!
+    @IBOutlet weak var notFirstName: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-   
+    @IBAction func lockTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func biometricTapped(_ sender: Any) {
+    }
+    
+    @IBAction func switchAccountPressed(_ sender: UIButton) {
+    }
+    @IBAction func createAccountPressed(_ sender: Any) {
+    }
 }
